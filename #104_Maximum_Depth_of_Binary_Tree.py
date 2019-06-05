7062068334552793cbc7bedaaf75c0523290ec1f
Yangidan <anyang49@gmail.com>
2019-06-04 20:51:58 -0900

    day 25

---
+++ #104_Maximum_Depth_of_Binary_Tree.py
@@ -0,0 +1,10 @@
+# day 26 2019-06-04 Tue
+# 104. Maximum Depth of Binary Tree
+class Solution:
+    def maxDepth(self, root):
+        if root is None: 
+            return 0 
+        else: 
+            left_D = self.maxDepth(root.left) 
+            right_D = self.maxDepth(root.right) 
+            return max(left_D, right_D) + 1 
